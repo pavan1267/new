298a25b81b2cabc4993611bab5dc9694121ac753
Pavan N <pavann.n@gmail.com>
2012-05-21 21:37:20 -0400

    Initial Commit

---
+++ new/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  new
+//
+//  Created by Pavan Narasimhaprasad on 5/21/12.
+//  Copyright (c) 2012 Stony Brook University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
